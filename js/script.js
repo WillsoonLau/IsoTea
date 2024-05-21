085a6c631a6de96753e69ac5faee6f90508a0f71
Wilson <magicalist12@gmail.com>
2024-05-21 19:16:54 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-let indexSlide = 0;
-
-function nextSlide() {
-    showSlide(indexSlide += 1);
-}
-
-function previousSlide() {
-    showSlide(indexSlide -= 1);
-}
-
-function showSlide(index) {
-    let listImage = document.getElementsByClassName('footer-banner-image');
-
-    if (index > listImage.length) indexSlide = 1;
-    if (index < 1) indexSlide = listImage.length;
-    for (let i = 0; i < listImage.length; i++) {
-        listImage[i].style.display = 'none';
-    }
-    listImage[indexSlide - 1].style.display = 'block';
-}
 
 document.addEventListener('DOMContentLoaded', function() {
     const headerNavButtons = document.querySelectorAll('header .nav-button');
@@ -34,4 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+    
+    let indexSlide = 0;
+
+    function nextSlide() {
+        showSlide(indexSlide += 1);
+    }
+    
+    function previousSlide() {
+        showSlide(indexSlide -= 1);
+    }
+    
+    function showSlide(index) {
+        let listImage = document.getElementsByClassName('footer-banner-image');
+    
+        if (index > listImage.length) indexSlide = 1;
+        if (index < 1) indexSlide = listImage.length;
+        for (let i = 0; i < listImage.length; i++) {
+            listImage[i].style.display = 'none';
+        }
+        listImage[indexSlide - 1].style.display = 'block';
+    }
+    setInterval(nextSlide, 2000);
 });
